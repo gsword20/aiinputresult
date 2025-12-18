7574610dc48d744f8c4c4e11b3b2a45e853fb9a6
GJ <ganjianus@gmail.com>
2025-12-18 20:51:18 +0800

    Initial Commit

---
+++ Aiinputresult/AiinputresultApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiinputresultApp.swift
+//  Aiinputresult
+//
+//  Created by J G on 2025/12/18.
+//
+
+import SwiftUI
+
+@main
+struct AiinputresultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AiinputresultTests/AiinputresultTests.swift
@@ -0,0 +1,16 @@
+//
+//  AiinputresultTests.swift
+//  AiinputresultTests
+//
+//  Created by J G on 2025/12/18.
+//
+
+import Testing
+
+struct AiinputresultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
